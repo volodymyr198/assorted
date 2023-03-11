c0501e19ad8ebd16b154939b005ece6563908bc8
volodymyr198 <volodymyr.b.198@gmail.com>
2023-03-11 20:27:06 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/assorted">
             <App />
         </BrowserRouter>
     </React.StrictMode>
